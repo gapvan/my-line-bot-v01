8c5c3583a6db772c54621b0691fe8758d1948aac
gapvan <joingap.gap@gmail.com>
2019-05-10 15:11:35 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,20 +14,21 @@ def hello():
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+##    # get X-Line-Signature header value
+##    signature = request.headers['X-Line-Signature']
+##
+##    # get request body as text
+##    body = request.get_data(as_text=True)
+##    app.logger.info("Request body: " + body)
+##
+##    # handle webhook body
+##    try:
+##        handler.handle(body, signature)
+##    except InvalidSignatureError:
+##        abort(400)
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
+    if request.method == 'POST':
+        return 'OK'
     
 
 @handler.add(MessageEvent, message=TextMessage)

05d77dacf12af62cf3a4d0d060528dd2e61fba04
gapvan <joingap.gap@gmail.com>
2019-05-16 15:11:02 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -48,8 +48,8 @@ def handle_message(event):
 ##        print(rich_menu_id)
     if text == 'profile':
         #if isinstance(event.source, SourceUser):
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage(text="ok"))
+##        line_bot_api.reply_message(
+##            event.reply_token,TextSendMessage(text="ok"))
         line_bot_api.reply_message(
             event.reply_token,TextSendMessage(text="บ้าบอ"))
         line_bot_api.reply_message(

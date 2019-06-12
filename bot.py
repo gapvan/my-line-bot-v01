5bf0301d54a2c5caf3f8dd3de5c2c75ae8fc0f27
gapvan <joingap.gap@gmail.com>
2019-06-12 14:45:46 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 keep_uid = ""
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-users = [[oorben,U60e0f8b22c313b3971d50c2bce9dbaa9],[gap,U9f6b4dfa2e30a22ad6a282dc34a86de2]]
+users = [['oorben','U60e0f8b22c313b3971d50c2bce9dbaa9'],['gap','U9f6b4dfa2e30a22ad6a282dc34a86de2']]
 @app.route("/")
 def hello():
     return "Hello World!"
